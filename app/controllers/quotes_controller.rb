7f5efb8394b884999599c9f960248a9fe7755e24
Ryan Yogan <ryanyogan@gmail.com>
2013-02-04 01:06:42 -0600

    Basic app to use with rulers

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,8 @@
+# best_qoutes/app/controllers/quotes_controller.rb
+
+class QuotesController < Rulers::Controller
+  def a_quote
+    "There is nothing either good or bad " +
+      "but thinking makes it so."
+  end
+end
