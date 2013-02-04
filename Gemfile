0f476b16d6548d977cfc3e167cd411205f110f69
Ryan Yogan <ryanyogan@gmail.com>
2013-02-04 13:42:42 -0600

    Added basic features to feed off of rails_api [rulers for now]

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 # best_quotes/Gemfile
 source :rubygems
 gem 'rulers', path: "../rails_api"
+
+group :development do
+  gem 'rerun'
+end
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,10 +1,31 @@
 # best_qoutes/app/controllers/quotes_controller.rb
 
 class QuotesController < Rulers::Controller
-  def a_quote
-    "There is nothing either good or bad " +
-      "but thinking makes it so."
+
+  def index
+    quotes = FileModel.all
+    render :index, quotes: quotes
   end
+
+  def new_quote
+    attrs = {
+      "submitter" => "Jack Frost",
+      "quote" => "Suck my dick",
+      "attribution" => "Ron Jeremy"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
+  def a_quote
+    render :a_quote, noun: :winking
+  end
+
+  def quote_1
+    quote_1 = FileModel.find 1
+    render :quote, obj: quote_1
+  end
+
 
   def exception
     raise "The shit has officially hit the fan!"
